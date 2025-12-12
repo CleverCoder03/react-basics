c059574760e754d4e819e327102b83fb11e8051e
Senpai <clevercoder0307@gmail.com>
2025-12-13 00:32:05 +0530

    add different theme  modes using shadcn

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
-
+import { Button } from "../components/ui/button"
+import { ModeToggle } from "../components/mode-toggle.tsx"
+ 
 function App() {
 
   return (
     <div className='flex justify-center items-center h-dvh'>
-      <h1 className='text-6xl font-bold'>Created using bun</h1>
+      <div>
+        <h1>Counter</h1>
+        <div className='absolute top-0 right-0 m-5'>
+          <ModeToggle />
+        </div>
+        <div>
+          <Button variant="outline">Button</Button>
+        </div>
+      </div>
     </div>
   )
 }
