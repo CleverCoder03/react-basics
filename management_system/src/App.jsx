8e5360611f27abaf71c390c1bd504629bbb7d343
Senpai <clevercoder0307@gmail.com>
2025-12-13 23:07:54 +0530

    add dark mode

--- management_system/src/App.jsx
+++ management_system/src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
-import { Button } from "../components/ui/button"
+import "./App.css";
+import { Button } from "../components/ui/button";
+import {ModeToggle} from "../components/mode-toggle"
 
 function App() {
-
   return (
     <>
-      <div className="flex justify-center items-center h-dvh">
-      <Button variant="outline">Senpai</Button>
+    <div className="fixed top-0 right-0 p-5">
+      <ModeToggle />
     </div>
+      <div className="flex justify-center items-center h-dvh">
+        <Button variant="outline">Senpai</Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
