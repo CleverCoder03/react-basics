64d6707fb895de6ec121ae0a8811f93520a2fcd9
Senpai <clevercoder0307@gmail.com>
2025-12-13 17:16:37 +0530

    Made controller working

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,8 +2,11 @@ import "./App.css";
 import { Button } from "../components/ui/button";
 import { ModeToggle } from "../components/mode-toggle.tsx";
 import { Input } from "../components/ui/input"
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [definedCount, setDefinedCount] = useState(0)
   return (
     <div className="flex justify-center items-center h-dvh">
       <div className="absolute top-0 right-0 m-5">
@@ -12,16 +15,16 @@ function App() {
 
       <div className="flex flex-col justify-center items-center gap-8">
         <h1 className="text-4xl font-semibold">Counter</h1>
-        <h2>Current count : 0</h2>
+        <h2>Current count : {count}</h2>
         <div className="flex flex-col gap-2">
-          <div className="flex gap-2">
-            <Button variant="outline">Increment</Button>
-            <Button variant="outline">Decrement</Button>
-            <Button variant="outline">Reset</Button>
+          <div className="flex gap-2 [&_button]:cursor-pointer">
+            <Button variant="outline" onClick={() => setCount(prev => prev+1)}>Increment</Button>
+            <Button variant="outline" onClick={() => setCount(prev => prev !== 0? prev - 1: 0)}>Decrement</Button>
+            <Button variant="outline" onClick={() => (setCount(0), setDefinedCount(0))}>Reset</Button>
           </div>
           <div className="flex gap-2">
-            <Input placeholder="Enter Number" className="outline-none focus:outline-none focus:ring-0" />
-            <Button variant="outline">Set as 0</Button>
+            <Input placeholder="Enter Number" className="outline-none focus:outline-none focus:ring-0" value={definedCount} onChange={(e) => setDefinedCount(e.target.value)} />
+            <Button variant="outline" className="cursor-pointer" onClick={() => (setCount(definedCount), setDefinedCount(0))}>Set as {definedCount}</Button>
           </div>
         </div>
       </div>
