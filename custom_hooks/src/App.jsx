8c1a70dcd4c5bc33ba45fff2ac34282d4afc50ec
Senpai <clevercoder0307@gmail.com>
2025-12-18 14:51:28 +0530

    initial project setup

---
+++ custom_hooks/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='h-dvh w-dvw bg-[#222]'>
+      <h1 className='text-white'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ custom_hooks/src/data/products.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id: 1,
+        name: "Wireless Earbuds",
+        price: 99.99
+    },
+    {
+        id: 2,
+        name: "Bluetooth Headphones",
+        price: 149.99
+    },
+    {
+        id: 3,
+        name: "Smart Watch",
+        price: 199.99
+    },
+    {
+        id: 4,
+        name: "Portable Speaker",
+        price: 79.99
+    },
+    {
+        id: 5,
+        name: "Wireless Charger",
+        price: 39.99
+    },
+    {
+        id: 6,
+        name: "Noise Cancelling Earphones",
+        price: 129.99
+    }
+];
